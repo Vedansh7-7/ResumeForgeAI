6215d1d8eb1d9e0c6e7b852b09b3833dd08a681a
Vedansh7-7 <adm.shajapur1@gmail.com>
2026-05-20 18:18:49 +0530

    Combined the main with parsing script.

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+from parse import parsePdf
 def main():
-    print("Hello from new-project!")
-
+    
+    # Reading a file
+    with open(".\processing_files\output.txt", "r", encoding="utf-8") as file:
+        content = file.read()
+    print(content)
 
 if __name__ == "__main__":
-    main()
+    print("Hello from new-project!\n\n")
+    file = input("Enter file name after adding it in the .\pdf: ")
+    result = parsePdf(file)
+    if result == r"Extracted into .\processing_files\output.txt":
+        main()
+    else:
+        print("Please check your pdf, we are not able to see any pages in it.")
--- parse.py
+++ parse.py
@@ -5,15 +5,31 @@ def parsePdf(file):
     file = f".\pdf\{file}.pdf"
     reader = PdfReader(file)
     number_of_pages = len(reader.pages)
-    if number_of_pages != 1:
+    if number_of_pages > 1:
+        return "No_Pages_Found"
+    elif number_of_pages != 1:
         print(f"There are {number_of_pages} pages in pdf.\nReading the first page...\n\n")
     else:
         print(f"There is only {number_of_pages} page in pdf.\nReading the page...\n\n")
-    page = reader.pages[0]
-    text = page.extract_text()
+
+    for i in range(number_of_pages):
+        page = reader.pages[i]
+        text = page.extract_text()
+
+        if i == 0:
+            # Writing to the file
+            with open(".\processing_files\output.txt", "w", encoding="utf-8") as file:
+                file.write(f"\n________ Page {i+1} _________\n")
+                file.write(text)
+        else:
+            # Appending to the file
+            with open(".\processing_files\output.txt", "a", encoding="utf-8") as file:
+                file.write(f"\n________ Page {i+1} _________\n")
+                file.write(text)
+
+    return "Extracted into .\processing_files\output.txt"
 
 
-
-if __name__ == "__main__":
-    file = input("Enter file name after adding it in the .\pdf: ")
-    parsePdf(file)
+# if __name__ == "__main__":
+#     file = input("Enter file name after adding it in the .\pdf: ")
+#     parsePdf(file)
