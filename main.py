bea564e8b3b875be15600b2dee47680167e63fa6
Vedansh7-7 <adm.shajapur1@gmail.com>
2026-05-23 21:15:14 +0530

    wroking on resume_builder.py

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
-import subprocess
-from parse import parsePdf
+# import subprocess
+# from parse import parsePdf
 
-def main():
+# def main():
     
-    with open("./processing_files/output.txt", "r", encoding="utf-8") as file:
-        content = file.read()
-    print(f"Characters in the parsed text = {len(content)}")
+#     with open("./processing_files/output.txt", "r", encoding="utf-8") as file:
+#         content = file.read()
+#     print(f"Characters in the parsed text = {len(content)}")
 
-if __name__ == "__main__":
-    print("Hello from new-project!\n\n")
-    file = input("Enter file name after adding it in the .\pdf: ")
-    result = parsePdf(file)
-    if result == r"Extracted into ./processing_files/output.txt":
-        main()
-    else:
-        print("Please check your pdf, we are not able to see any pages in it.")
-    result = subprocess.run(['python', 'section_extractor.py'], capture_output=True, text=True)
-    print(result.stdout)
+# if __name__ == "__main__":
+#     print("Hello from new-project!\n\n")
+#     file = input("Enter file name after adding it in the .\pdf: ")
+#     result = parsePdf(file)
+#     if result == r"Extracted into ./processing_files/output.txt":
+#         main()
+#     else:
+#         print("Please check your pdf, we are not able to see any pages in it.")
+#     result = subprocess.run(['python', 'section_extractor.py'], capture_output=True, text=True)
+#     print(result.stdout)
+
+from user.db.db_queries import get_achievements, get_courses, get_education, get_experience, get_personal_info, get_positions, get_projects, get_skills, get_user_by_name
+
+user = input("What is your username?\n")
+row = get_user_by_name(user)
+
+if not row:
+    print("User not found.")
+else:
+    user_id = row["id"]
+
+    functions = [get_achievements, get_courses, get_education, get_experience, 
+                 get_personal_info, get_positions, get_projects, get_skills]
+    name_of_functions = ['achievements', 'courses', 'education', 'experience', 
+                         'personal info', 'positions', 'projects', 'skills']
+
+    print(f"Printing details of {user}: {row}\n" + "-"*50)
+
+    for idx in range(len(functions)):
+        print(f"User's {name_of_functions[idx]}: {functions[idx](user_id)}")
